df19e9ba2eca29d44ddc2e25a616d2fcfb67b909
GhoRid <pgh5489@gmail.com>
2024-01-07 23:33:46 +0900

    feature: 현재 위치 불러오기

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    //foreground permission: 앱이 foreground에 있을 때만 위치 정보를 가져옴
+    //background permission: 앱이 background에 있을 때도 위치 정보를 가져옴
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    } else {
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(
+        {
+          longitude,
+          latitude,
+        },
+        { useGoogleMaps: "false" }
+      );
+      setCity(location[0].city);
+    }
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.city}>
-        <Text style={styles.cityname}>광주광역시</Text>
+        <Text style={styles.cityname}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
